9a40ac7cb039423d26f03e1715a84cc4cd696d80
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2026-03-13 02:33:26 +0530

    feat: add tanstack query with QueryClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,25 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import App from "./App.tsx";
 import { AuthProvider } from "./lib/auth-context";
 import "@uppy/react/css/style.css";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>,
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>,
 );
